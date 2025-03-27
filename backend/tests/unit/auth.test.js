32af3cb997400f79875f7fdbbc28f9ec1bf65f38
Anna0131 <fnchiang6633@gmail.com>
2025-03-27 18:59:47 +0800

    0327 test one

--- backend/tests/unit/auth.test.js
+++ backend/tests/unit/auth.test.js
@@ -4,12 +4,6 @@ const assert = require('node:assert');
 const Auth = require("../../lib/auth");
 const auth = new Auth();
 //fine
-<<<<<<< HEAD
-
-
-=======
-
->>>>>>> 4e5a998e0fc6055ac5b1f410abf450ab2015b420
 test('Auth can successfully verify the identity.', async (t) => {
 
   const mock_next = () => {
